c297b0ac0ea64d1fae85ad5f3cd257cf8a7b6b5d
David Bueno <davidbuenodbc@gmail.com>
2023-04-28 12:42:20 +0200

    wip create project
    wip create project to do list in swifUI

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by David Bueno Castro on 28/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
